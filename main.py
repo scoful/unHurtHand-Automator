d5a54358fa6ab864b0d9570950be789dff6091db
scoful <1269717999@qq.com>
2019-12-27 12:28:25 +0800

    代码逻辑调整了一下，支持，自动领完积分后再自动去蚂蚁森林

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ if __name__ == '__main__':
     # 九宫锁解锁的点的坐标，可以通过Weditor来获取每个点的相对坐标
     lock_points = [(0.235, 0.575), (0.494, 0.571), (0.756, 0.859), (0.243, 0.863)]
     # 是否开启蚂蚁森林自动收能量任务
-    is_ant_forest_on = False
+    is_ant_forest_on = True
+    # 是否开启支付宝app领积分自动任务
+    is_zhi_fu_bao_gain_points_on = True
     # 是否开启支付宝app2019双11活动自动任务
     is_zhi_fu_bao_to_2019_on = False
     # 是否开启淘宝app2019双11活动自动任务
-    is_tao_bao_to_2019_on = True
-    # 是否开启支付宝app领积分自动任务
-    is_zhi_fu_bao_gain_points_on = False
+    is_tao_bao_to_2019_on = False
 
     instance = Automator(device, is_app_lock, lock_points, is_ant_forest_on, is_zhi_fu_bao_to_2019_on,
                          is_tao_bao_to_2019_on, is_zhi_fu_bao_gain_points_on)
